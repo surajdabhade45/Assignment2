35e9d45e4d658a0758af73267128818c46d00e89
surajdabhade45 <116947563+surajdabhade45@users.noreply.github.com>
2025-04-28 14:26:18 +0530

    Add files via upload

---
+++ Assign2 Task1.py
@@ -0,0 +1,13 @@
+#Check is a Number is Even or odd
+from xml.dom.minidom import ProcessingInstruction
+
+a= int
+a = input (" Enter the number: ")
+#b = input( " Enter the number: ")
+
+if int(a) % 2 == 0:
+    print(a, " is an even number")
+else:
+    print(a, " is an odd number")
+
+
---
+++ Assign2 Task2.py
@@ -0,0 +1,6 @@
+#Sum of Intergers from 1 to 50 using for loop
+sum = 0
+for num in range(1, 51):
+    sum = sum + num
+
+print ("The sum of numbers from 1 to 50 is: ", sum)
